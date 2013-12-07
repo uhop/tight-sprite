d0ef1780973a23d54ee66513764ebbc0faaae9eb
Eugene Lazutkin <eugene@lazutkin.com>
2013-12-07 13:17:08 -0600

    Added "silent" mode.

--- palletizing.js
+++ palletizing.js
@@ -4,10 +4,19 @@
 var maxRect = require("./lib/solvers/maxRect");
 var Pallet  = require("./lib/Pallet");
 var getArea = require("./lib/utils/getArea");
+var getTotalArea = require("./lib/utils/getTotalArea");
 
 
-module.exports = function(rectangles){
+module.exports = function(rectangles, options){
+	options = options || {};
+
+	if(!options.silent){
+		console.log("Packing " + rectangles.length +
+			" rectangles using the maximal rectangles algorithm.");
+	}
+
 	var pallet = new Pallet(rectangles),
+		totalArea = options.silent ? 0 : getTotalArea(rectangles),
 		bestArea = Infinity, bestLayout = null;
 
 	for(;;){
@@ -17,6 +26,11 @@ module.exports = function(rectangles){
 			if(area < bestArea){
 				bestArea = area;
 				bestLayout = layout;
+				if(!options.silent){
+					console.log("Found rectangle " + pallet.w +
+						" by " + pallet.h + " wasting only " +
+						(area - totalArea) + " pixels.");
+				}
 			}
 			if(!pallet.next(area)){
 				break;
@@ -26,6 +40,11 @@ module.exports = function(rectangles){
 				break;
 			}
 		}
+	}
+
+	if(!options.silent){
+		console.log("The best solution with the maximal rectangles algorithm wasted " +
+			(bestArea - totalArea) + " pixels.");
 	}
 
 	return {area: bestArea, layout: bestLayout, rectangles: rectangles};
--- windowing.js
+++ windowing.js
@@ -14,40 +14,68 @@ var isSymmetric  = require("./lib/utils/isSymmetric");
 
 module.exports = function(rectangles, strategies, options){
 	options = options || {};
+
+	if(!options.silent){
+		console.log("Packing " + rectangles.length + " rectangles with " +
+			strategies.length + " strategies using the envelope-based algorithm.");
+	}
+
 	var depth = options.depth || 3,
-		finalDepth = options.finalDepth || depth,
-		suppressSymmetry = options.suppressSymmetry;
+		finalDepth = options.finalDepth || depth;
 
 	var totalArea = getTotalArea(rectangles),
 		bestArea = Infinity, bestLayout = null, bestRectangles = null;
 
-	strategies.some(function(produceScore){
-		var result = windowOpt(rectangles, produceScore, depth, finalDepth),
-			area = getArea(rectangles, result.layout);
-		if(area < bestArea && result.layout){
-			bestArea = area;
-			bestLayout = result.layout;
-			bestRectangles = rectangles.slice(0);
-		}
-		return bestArea === totalArea;
-	});
+	packing: {
+		strategies.some(function(produceScore, index){
+			if(!options.silent){
+				console.log("Trying strategy #" + (index + 1) + " ...");
+			}
+			var result = windowOpt(rectangles, produceScore, depth, finalDepth),
+				area = getArea(rectangles, result.layout);
+			if(!options.silent){
+				console.log("Wasted " + (area - totalArea) + " pixels.");
+			}
+			if(result.layout && area < bestArea){
+				bestArea = area;
+				bestLayout = result.layout;
+				bestRectangles = rectangles.slice(0);
+			}
+			return bestArea === totalArea;
+		});
 
-	if(bestArea === totalArea || suppressSymmetry || isSymmetric(rectangles)){
-		return {area: bestArea, layout: bestLayout, rectangles: bestRectangles};
+		if(bestArea === totalArea || options.suppressSymmetry || isSymmetric(rectangles)){
+			break packing;
+		}
+
+		if(!options.silent){
+			console.log("Trying a rotated solution.");
+		}
+
+		var rotatedRectangles = rotate(rectangles);
+
+		strategies.some(function(produceScore){
+			if(!options.silent){
+				console.log("Trying strategy #" + (index + 1) + " ...");
+			}
+			var result = windowOpt(rotatedRectangles, produceScore, depth, finalDepth),
+				area = getArea(rectangles, result.layout);
+			if(!options.silent){
+				console.log("Wasted " + (area - totalArea) + " pixels.");
+			}
+			if(result.layout && area < bestArea){
+				bestArea = area;
+				bestLayout = rotateLayout(result.layout);
+				bestRectangles = unrotate(rectangles);
+			}
+			return bestArea === totalArea;
+		});
 	}
 
-	var rotatedRectangles = rotate(rectangles);
-
-	strategies.some(function(produceScore){
-		var result = windowOpt(rotatedRectangles, produceScore, depth, finalDepth),
-			area = getArea(rectangles, result.layout);
-		if(area < bestArea && result.layout){
-			bestArea = area;
-			bestLayout = rotateLayout(result.layout);
-			bestRectangles = unrotate(rectangles);
-		}
-		return bestArea === totalArea;
-	});
+	if(!options.silent){
+		console.log("The best solution with the envelope-based algorithm wasted " +
+			(bestArea - totalArea) + " pixels.");
+	}
 
 	return {area: bestArea, layout: bestLayout, rectangles: bestRectangles};
 };
